71bea1c83b0017ebe5ca1758132833c98dcf226d
GuiBratiliere <110342103+GuiBratiliere@users.noreply.github.com>
2024-12-06 12:07:29 -0300

    Add files via upload

--- faram_consultoria/js/scripts.js
+++ faram_consultoria/js/scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
     $('.mobile_btn').on('click', function(){
-        $('#mobile_menu').toggclass('active');
-        $('.mobile_btn').find('i').toggclass('fa-x');
-    })
+        $('#mobile_menu').toggleClass('active');
+        $('.mobile_btn').find('i').toggleClass('fa-x');
+    });
 
 
     const sections = $('section');
     const navItems = $('.nav_item');
-    $
+$   
     (window).on('scroll', function(){
         const header = $('header');
         let activesectionindex = 0;
         const scrollposition = $(window).scrollTop() - header.outerHeight();
 
         if (scrollposition <= 0) {
-            header.css('box-shadow', 'none')
+            header.css('box-shadow', 'none');
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
@@ -28,7 +28,7 @@ $(document).ready(function(){
                 activesectionindex = i
                 return false;
             }
-        })
+        });
         
         navItems.removeClass('active');
         $(navItems[activesectionindex]).addClass('active');
